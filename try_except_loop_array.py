6408c431b0c45eb56e8c3f71c47456b8e2edc8c2
LianKarylGarcia <liankarylg@gmail.com>
2024-10-23 06:57:40 -0400

    Using array dictionary to store user infos

--- try_except_loop_array.py
+++ try_except_loop_array.py
@@ -1,3 +1,5 @@
+user_infos = {}
+
 while True: # First loop
     try:
         while True: # Loop until the valid name is provided
@@ -13,14 +15,19 @@ while True: # First loop
         while True: # Loop until the valid age is provided
             age = int(input("Please input age: "))
 
-            if age >= 18: # Setting condition for a valid age
+            if age >= 18: # Setting condition for a valid age # len does not work with int 
                 break # To break the inner loop if age is valid
             
             else: 
                 print("Age must be 18 and above")
+
+        user_infos[name] = { # To store user infos in the dictionary
+            "name" : name,
+            "age" : age
+        }
         
         break # To break the outer loop if both inputs are valid
-            
+
     except ValueError:
         print("Input invalid. Please input correct values.")
 
