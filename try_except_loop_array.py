698235f9ae17217ad31ed1b85f7587e6be64c467
LianKarylGarcia <liankarylg@gmail.com>
2024-10-23 05:37:08 -0400

    len is not working

--- try_except_loop_array.py
+++ try_except_loop_array.py
@@ -1,7 +1,12 @@
 while True:
     try:
         name = input("Please input name: ")
+
+        if len(name) <= 5:
+        
         age = int(input("Please input age: "))
+
+        if len(age) >= 18:
+
     except:
         print("Input invalid")
-        
